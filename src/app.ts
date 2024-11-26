61d079e3ea5b1d5facf482970081bd1f0906dca3
Guilherme <guifalcao2017@gmail.com>
2024-11-26 15:13:46 -0300

    retirada de comentarios

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import ParametrosPorGrupo from "./classes/parametrosPorGrupo";
 
-//Obtendo os registros da tabela
 const XLSX = require('xlsx');
 const arquivo = XLSX.readFile('/home/ti-1562/Documentos/Guilherme/applab/src/planilhas/relatorioCGP.xlsx');
 const tabela = arquivo.Sheets[arquivo.SheetNames[0]];
