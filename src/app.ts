1755510ed798d3b5b9fbb6027b7624295da69324
Guilherme <guifalcao2017@gmail.com>
2024-11-27 09:25:14 -0300

    separação do script do app para um script para instanciar a classe feita anteriormente  e ler os dados da planilha para depois exportar

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,3 @@
-import ParametrosPorGrupo from "./classes/parametrosPorGrupo";
+import parametrosPorGrupo from "./leitura/leituraCGP";
 
-const XLSX = require('xlsx');
-const arquivo = XLSX.readFile('./src/planilhas/relatorioCGP.xlsx');
-const tabela = arquivo.Sheets[arquivo.SheetNames[0]];
-const dadosTabela = XLSX.utils.sheet_to_json(tabela);
-
-
-const parametrosPorGrupo = new ParametrosPorGrupo()
-
-parametrosPorGrupo.processarDados(dadosTabela)
-
-console.log(parametrosPorGrupo.getQPG())
+console.log(parametrosPorGrupo.getGrupos())
---
+++ src/leitura/leituraCGP.ts
@@ -0,0 +1,13 @@
+import ParametrosPorGrupo from "../classes/parametrosPorGrupo";
+
+const XLSX = require('xlsx');
+const arquivo = XLSX.readFile('./src/planilhas/relatorioCGP.xlsx');
+const tabela = arquivo.Sheets[arquivo.SheetNames[0]];
+const dadosTabela = XLSX.utils.sheet_to_json(tabela);
+
+
+const parametrosPorGrupo = new ParametrosPorGrupo()
+
+parametrosPorGrupo.processarDados(dadosTabela)
+
+export default parametrosPorGrupo
