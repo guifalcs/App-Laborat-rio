af701d88d591d21c5bbace183a2a28f09eb99e54
Guilherme <guifalcao2017@gmail.com>
2024-12-05 15:34:26 -0300

    Redirecionando para a página mar a partir do container do modulo na homeModulos

--- cliente/src/app/app.routes.ts
+++ cliente/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeModulosComponent } from './paginas/home-modulos/home-modulos.component';
+import { MarComponent } from './paginas/modulos/mar/mar.component';
 
 export const routes: Routes = [
-  {path: '', component: HomeModulosComponent}
+  {path: '', component: HomeModulosComponent},
+  {path: 'mar', component: MarComponent},
 ];
--- cliente/src/app/paginas/home-modulos/home-modulos.component.ts
+++ cliente/src/app/paginas/home-modulos/home-modulos.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../componentesPag/header/header.component';
 import { FooterComponent } from '../../componentesPag/footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-modulos',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, RouterLink],
   templateUrl: './home-modulos.component.html',
   styleUrl: './home-modulos.component.scss'
 })
