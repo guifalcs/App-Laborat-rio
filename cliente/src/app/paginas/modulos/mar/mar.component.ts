4ff4976aeff9815d9e191146ae67e1738b7df805
Guilherme <guifalcao2017@gmail.com>
2024-12-05 15:39:24 -0300

    Inicio da criação da pagina do modulo MAR

--- cliente/src/app/paginas/modulos/mar/mar.component.ts
+++ cliente/src/app/paginas/modulos/mar/mar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../componentesPag/header/header.component';
+import { FooterComponent } from '../../../componentesPag/footer/footer.component';
 
 @Component({
   selector: 'app-mar',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './mar.component.html',
   styleUrl: './mar.component.scss'
 })
