0271d5a708a7583fb696b618a740196a0b21e352
Guilherme <guifalcao2017@gmail.com>
2024-12-05 15:26:23 -0300

    criação do component para o módulo MAR

---
+++ cliente/src/app/paginas/modulos/mar/mar.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MarComponent } from './mar.component';
+
+describe('MarComponent', () => {
+  let component: MarComponent;
+  let fixture: ComponentFixture<MarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MarComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
