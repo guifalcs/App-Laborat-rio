dc236d7afd8a4f9407e22a4b4778e7ccece856a0
Guilherme <guifalcao2017@gmail.com>
2024-12-05 11:29:03 -0300

    Criação do footer

--- cliente/src/app/paginas/home-modulos/home-modulos.component.ts
+++ cliente/src/app/paginas/home-modulos/home-modulos.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../componentesPag/header/header.component';
+import { FooterComponent } from '../../componentesPag/footer/footer.component';
 
 @Component({
   selector: 'app-home-modulos',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './home-modulos.component.html',
   styleUrl: './home-modulos.component.scss'
 })
