05bfd5d5fd8d55112a070b4a1cc32fd39268cac6
Guilherme <guifalcao2017@gmail.com>
2024-12-06 14:19:07 -0300

    Criação do dotenv e configuração da porta do servidor nele

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,11 @@ import express from 'express'
 import { router } from './router'
 
 const app = express()
+const port = process.env.SERVER_PORT || 8081
 
 app.use(express.json())
 app.use(router)
 
-app.listen(8081, () => {
-    console.log('Servidor rodando na porta 8081')
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
 })
