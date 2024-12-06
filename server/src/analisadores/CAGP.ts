988f9d1a7a040791362709ffa7a6d8ae431ca3b4
Guilherme <guifalcao2017@gmail.com>
2024-12-06 13:46:22 -0300

    ajuste na logica do get AGP

--- server/src/analisadores/CAGP.ts
+++ server/src/analisadores/CAGP.ts
@@ -10,94 +10,84 @@ export default class CAGP {
 
       this.registros = registros;
 
-      // let gp: Array<GP> = [];
+      let gp: Array<GP> = [];
   
-      // for (let i = 0; i < registros.length; i++) {
-      //   gp.push({
-      //     grupo: registros[i]["Grupo"],
-      //     parametros: registros[i]["Parâmetros"],
-      //   });
-      // }
+      for (let i = 0; i < registros.length; i++) {
+        gp.push({
+          grupo: registros[i]["Grupo"],
+          parametros: registros[i]["Parâmetros"],
+        });
+      }
   
-      // let grupos: Array<string> = [];
+      let grupos: Array<string> = [];
   
-      // let gruposComParametros: { [key: string]: { [key: string]: number } } = {}; 
+      let gruposComParametros: { [key: string]: { [key: string]: number } } = {}; 
   
-      // gp.forEach(({ grupo, parametros }) => {
-      //   if (!grupos.includes(grupo)) {
-      //     grupos.push(grupo);
-      //   }
+      gp.forEach(({ grupo, parametros }) => {
+        if (!grupos.includes(grupo)) {
+          grupos.push(grupo);
+        }
   
-      //   if (!gruposComParametros[grupo]) {
-      //     gruposComParametros[grupo] = {};
-      //   }
+        if (!gruposComParametros[grupo]) {
+          gruposComParametros[grupo] = {};
+        }
   
-      //     const parametrosComMarcador = parametros.replace(/(\d),(\d)/g, "$1_$2");
+          const parametrosComMarcador = parametros.replace(/(\d),(\d)/g, "$1_$2");
   
-      //     const listaParametros = parametrosComMarcador
-      //       .split(",")
-      //       .map((param: any) => param.trim());
+          const listaParametros = parametrosComMarcador
+            .split(",")
+            .map((param: any) => param.trim());
   
-      //     const listaParametrosCorrigidos = listaParametros.map((param: any) =>
-      //       param.replace(/_/g, ",")
-      //     );
+          const listaParametrosCorrigidos = listaParametros.map((param: any) =>
+            param.replace(/_/g, ",")
+          );
   
-      //     listaParametrosCorrigidos.forEach((parametro: any) => {
-      //       if (!gruposComParametros[grupo][parametro]) {
-      //         gruposComParametros[grupo][parametro] = 0;
-      //       }
-      //       gruposComParametros[grupo][parametro]++;
-      //     });
+          listaParametrosCorrigidos.forEach((parametro: any) => {
+            if (!gruposComParametros[grupo][parametro]) {
+              gruposComParametros[grupo][parametro] = 0;
+            }
+            gruposComParametros[grupo][parametro]++;
+          });
         
-      // });
+      });
   
-      // let porcentagens: { [key: string]: { [key: string]: number } } = {}; 
+      let porcentagens: { [key: string]: { [key: string]: number } } = {}; 
   
-      // grupos.forEach((grupo) => {
-      //   let totalParametrosGrupo = 0;
-      //   for (let parametro in gruposComParametros[grupo]) {
-      //     totalParametrosGrupo += gruposComParametros[grupo][parametro];
-      //   }
+      grupos.forEach((grupo) => {
+        let totalParametrosGrupo = 0;
+        for (let parametro in gruposComParametros[grupo]) {
+          totalParametrosGrupo += gruposComParametros[grupo][parametro];
+        }
   
-      //   let parametrosPorGrupo = [];
-      //   for (let parametro in gruposComParametros[grupo]) {
-      //     let count = gruposComParametros[grupo][parametro];
-      //     let percentage = (count / totalParametrosGrupo) * 100;
-      //     parametrosPorGrupo.push({
-      //       parametro,
-      //       percentage: parseFloat(percentage.toFixed(2)), 
-      //     });
-      //   }
+        let parametrosPorGrupo = [];
+        for (let parametro in gruposComParametros[grupo]) {
+          let count = gruposComParametros[grupo][parametro];
+          let percentage = (count / totalParametrosGrupo) * 100;
+          parametrosPorGrupo.push({
+            parametro,
+            percentage: parseFloat(percentage.toFixed(2)), 
+          });
+        }
   
-      //   parametrosPorGrupo.sort((a, b) => b.percentage - a.percentage);
+        parametrosPorGrupo.sort((a, b) => b.percentage - a.percentage);
   
-      //   porcentagens[grupo] = {};
-      //   parametrosPorGrupo.forEach(({ parametro, percentage }) => {
-      //     porcentagens[grupo][parametro] = percentage;
-      //   });
+        porcentagens[grupo] = {};
+        parametrosPorGrupo.forEach(({ parametro, percentage }) => {
+          porcentagens[grupo][parametro] = percentage;
+        });
   
-      //   let parametrosOrdenados = Object.entries(gruposComParametros[grupo])
-      //     .sort(([, countA], [, countB]) => countB - countA)
-      //     .reduce((acc, [key, value]) => {
-      //       acc[key] = value;
-      //       return acc;
-      //     }, {} as { [key: string]: number });
+        let parametrosOrdenados = Object.entries(gruposComParametros[grupo])
+          .sort(([, countA], [, countB]) => countB - countA)
+          .reduce((acc, [key, value]) => {
+            acc[key] = value;
+            return acc;
+          }, {} as { [key: string]: number });
   
-      //   gruposComParametros[grupo] = parametrosOrdenados;
-      // });
+        gruposComParametros[grupo] = parametrosOrdenados;
+      });
 
-
-      // this.registros.map((registro: dadosCAGP) => {
-
-      //   let novoItem: agp = [registro["Referência"], registro["Grupo"], [registro["Parâmetros"]]];
-      //   this.agp.push(novoItem);
-
-      // })
-
-
-      // this.ppg = porcentagens;
-      // this.qpg = gruposComParametros;
-      // this.grupos = grupos
+      this.ppg = porcentagens;
+      this.qpg = gruposComParametros;
     }
 
     getPPG(){
@@ -185,6 +175,12 @@ export default class CAGP {
     }
 
     getAGP(){
+
+      this.registros.map((registro: dadosCAGP) => {
+        let novoItem: agp = [registro["Referência"], registro["Grupo"], [registro["Parâmetros"]]];
+        this.agp.push(novoItem);
+      })
+
       return this.agp
     }
 
