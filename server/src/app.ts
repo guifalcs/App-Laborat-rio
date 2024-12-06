a960cfeffdbd663aca9b41e0dca464e40176342c
Guilherme <guifalcao2017@gmail.com>
2024-12-06 13:44:15 -0300

    Refatoração do getGrupos no CAGP

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,6 @@ import fgp from "./leitores/leituraFGP";
 import cagp from "./leitores/leituraCAGP";
 import cfa from "./leitores/leituraCFA";
 
-console.log(fgp.getClientesPorFaixa('2024'))
-// console.log(cagp.getTopGrupos())
+// console.log(fgp.getClientesPorFaixa('2024'))
+console.log(cagp.getGrupos())
 // console.log(cfa.getTicketMedioAno('2024'))
