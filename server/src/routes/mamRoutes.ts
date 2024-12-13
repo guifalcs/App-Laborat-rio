d860a4b502ad36cfae8959fb6423fe834194f4c4
Guilherme <guifalcao2017@gmail.com>
2024-12-13 14:12:33 -0300

    correções

--- server/src/routes/mamRoutes.ts
+++ server/src/routes/mamRoutes.ts
@@ -7,7 +7,7 @@ const mamRoutes = express.Router();
 
 mamRoutes.get("/connect", async (req: Request, res: Response) => {
   try {
-    const qrCode = await conection.getCodeConection();
+    await conection.getCodeConection();
     res.json("QRCODE criado com sucesso!");
   } catch (error) {
     res.status(500).send("Erro ao gerar QR Code. " + error);
