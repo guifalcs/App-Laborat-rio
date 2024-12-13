60f6677aab0717c6ae630ccb5a8a393230731f5a
Guilherme <guifalcao2017@gmail.com>
2024-12-13 13:29:52 -0300

    Ta funcionando

--- server/src/MAM/conection.ts
+++ server/src/MAM/conection.ts
@@ -16,6 +16,25 @@ class WhatsAppConection {
 
     this.client = client;
   }
+
+  async getCodeConection(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      this.client.on("qr", (qr: any) => {
+        this.codigo = qr;
+        resolve(this.codigo);  
+      });
+
+      this.client.on("ready", () => {
+        console.log("Cliente está pronto!");
+      });
+
+      this.client.on("disconnected", (motivo: any) => {
+        console.log(`Cliente desconectado. Razão: ${motivo}`);
+      });
+
+      this.client.initialize();
+    });
+  }
 }
 
 export default WhatsAppConection;
--- server/src/routes/mamRoutes.ts
+++ server/src/routes/mamRoutes.ts
@@ -7,28 +7,15 @@ const conection = new WhatsAppConection();
 const mamRoutes = express.Router();
 
 mamRoutes.get("/connect", async (req: Request, res: Response) => {
-
-    console.log('vai plmds')
-
-  await conection.client.initialize()
-
-  conection.client.on('qr', (qr: any) => {
-    console.log('QR RECEIVED', qr);
-  });
-
   try {
-    const qrCodeImage = await qrcode.toBuffer(conection.codigo);
+    const qrCode = await conection.getCodeConection();
 
-    // conection.client.once("ready", () => {
-    //   res.redirect(
-    //     "https://wwebjs.dev/guide/creating-your-bot/authentication.html#remote-stores"
-    //   );
-    // });
+    const qrCodeImage = await qrcode.toBuffer(qrCode);
 
     res.setHeader("Content-Type", "image/png");
     res.send(qrCodeImage);
   } catch (error) {
-    res.status(500).send("Erro ao gerar QR Code.");
+    res.status(500).send("Erro ao gerar QR Code. " + error);
   }
 });
 
