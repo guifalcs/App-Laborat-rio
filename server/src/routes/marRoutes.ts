60d978bb00728ad73e317830e152961bad721f51
Guilherme <guifalcao2017@gmail.com>
2024-12-14 14:53:42 -0300

    Alteração da função getClientes ano para um generator e adaptação da sua rota

--- server/src/routes/marRoutes.ts
+++ server/src/routes/marRoutes.ts
@@ -1,9 +1,20 @@
 import express, {Request, Response} from 'express'
+import cfa from '../MAR/leitores/leituraCFA'
 
 const marRoutes = express.Router()
 
 marRoutes.get('/', (req: Request, res: Response) => {
-    res.send('Rota get no mar funcionando')
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Transfer-Encoding', 'chunked');
+    
+    const clientesGenerator = cfa.getClientesAno("2024")
+    let cliente;
+
+    for(cliente of clientesGenerator){
+        res.write(JSON.stringify(cliente) + '\n')
+    }
+
+    res.end()
 })
 
 export default marRoutes
