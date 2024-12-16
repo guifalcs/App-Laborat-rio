8935df96e57f958918ca6d231c725cf3e05673cc
Guilherme <guifalcao2017@gmail.com>
2024-12-16 14:15:27 -0300

    Inicio da estruturaçã do mam

--- cliente/src/app/paginas/modulos/mam/mam.component.ts
+++ cliente/src/app/paginas/modulos/mam/mam.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../componentesPag/header/header.component';
+import { FooterComponent } from '../../../componentesPag/footer/footer.component';
 
 @Component({
   selector: 'app-mam',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './mam.component.html',
   styleUrl: './mam.component.scss'
 })
