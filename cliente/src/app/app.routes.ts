47fd745f9af7cce9c7110c86eab7a4338e2166f5
Guilherme <guifalcao2017@gmail.com>
2024-12-16 14:10:04 -0300

    ajuste de rotas

--- cliente/src/app/app.routes.ts
+++ cliente/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeModulosComponent } from './paginas/home-modulos/home-modulos.component';
 import { MarComponent } from './paginas/modulos/mar/mar.component';
+import { MamComponent } from './paginas/modulos/mam/mam.component';
 
 export const routes: Routes = [
   {path: '', component: HomeModulosComponent},
   {path: 'mar', component: MarComponent},
+  {path: 'mam', component: MamComponent}
 ];
